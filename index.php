004f119826b1298c3dd6d904496a8abee770ae9e
flip-game <115126336+flip-game@users.noreply.github.com>
2022-10-05 21:02:42 -0500

    Main Page

---
+++ index.php
@@ -0,0 +1,50 @@
+
+
+
+<html>
+<head>
+
+<title>flip! | Landing</title>
+<link rel="icon" type="image/png" href="/storage/img/flip_normal.png">
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="flip! is an online interactive game platform.">
+<meta name="keywords" value="flip, flip!, roblox, blox, game">
+<meta name="author" content="flip! Limited">
+<meta property="og:image" content="/storage/img/flip_normal.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/storage/css/foundation.css?r=2">
+<link rel="stylesheet" href="/storage/css/landing.css?r=1">
+</head>
+
+<body>
+<video playsinline="" autoplay="" muted="" loop="" width="100%" height="100%">
+<source src="/storage/vid/flip_gameplay.mp4">
+</video>
+<div class="video-overlay"></div>
+<div class="min-header">
+<div class="grid-x grid-margin-x align-middle">
+<div class="auto cell">
+<!--<img src="/storage/img/brickedicon.png" width="175">-->
+</div>
+<div class="shrink cell right">
+</div>
+</div>
+</div>
+<div class="video-content">
+<div class="content-title"><img src="/storage/img/flip_normal.png" height="250px" width="250px"></div>
+<div class="content-subtitle">Community Building.</div>
+
+<div class="content-links auth-menu">
+<a href="/account/register" class="button-create-account">Get Started Today</a>
+<br>
+<br>
+<a href="/account/login" class="button-login" style="width:80px;">Login</a>
+</div>
+</div>
+<div class="site-footer"><span>© Copyright 2022 flip! Limited All rights reserved.</span></div>
+
+
+</body>
+</html>
